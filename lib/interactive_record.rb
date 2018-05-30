0be00fa04d8f21647725a021b07ae3bcae8f3538
Fiona Lapham <fionadlapham@gmail.com>
2018-05-30 20:30:03 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,9 +59,10 @@ class InteractiveRecord
   end
 
   def self.find_by(attri)
-    @column_name = attri.flatten[0]
-    @column_value = attri.flatten[1]
-    sql = "SELECT * FROM #{self.table_name} WHERE #{@column_name}  = #{@column_value}"
+    column_name = attri.flatten[0]
+    value = attri.flatten[1]
+    column_value = value.class == Fixnum ? value : "value"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{column_name}  = #{column_value}"
     DB[:conn].execute(sql)
   end
 

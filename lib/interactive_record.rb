97de2842529f082bc6eb1c58f193caac7ff753c5
Fiona Lapham <fionadlapham@gmail.com>
2018-05-30 20:16:04 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,7 +60,6 @@ class InteractiveRecord
 
   def self.find_by(attri)
     @column_name = attri.flatten[0].to_s
-    binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE #{@column_name} = #{@column_value}"
     DB[:conn].execute(sql)
   end
